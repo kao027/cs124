c48c07516c16f74cb70c441558b9676c2c73922b
Khadijah Olufayo <khadijahaolufayo@gmail.com>
2025-02-20 17:16:37 -0500

    Fixed randomness on Graph 1

--- progset1/pg1.cpp
+++ progset1/pg1.cpp
@@ -1,26 +1,26 @@
 #include <chrono>
 #include <vector>
-#include <bits/stdc++.h>
 #include <iostream>
+#include <cmath>
 
 int n = 3; // number of nodes
-void addEdge(std::vector<std::vector<std::pair<int,int>>> &adj, int u, int v, int w){
+void addEdge(std::vector<std::vector<std::pair<int,float>>> &adj, int u, int v, float w){
     adj[u].push_back({v,w});
     adj[v].push_back({u,w}); // because undirected
 }
 
 //graph type #1
-void generateComplete(std::vector<std::vector<std::pair<int,int>>> &adj){ //
+void generateComplete(std::vector<std::vector<std::pair<int,float>>> &adj){
     srand(time(0));
     for (int u = 0; u < n; u++){
         for (int v = u+1; v < n; v++){
-            int newWeight = rand() % 1 + 0;
+            float newWeight = static_cast<float>(rand()) / RAND_MAX; // generate number from 0 to 1
             addEdge(adj, u, v, newWeight);
         }
     }
 }
 
-void displayAdjList(std::vector<std::vector<std::pair<int,int>>>& adj) {
+void displayAdjList(std::vector<std::vector<std::pair<int,float>>>& adj) {
     for (int i = 0; i < adj.size(); i++) {
         std::cout << i << ": "; 
         for (auto &j : adj[i]) {
@@ -32,7 +32,7 @@ void displayAdjList(std::vector<std::vector<std::pair<int,int>>>& adj) {
 
 
 int main(){
-    std::vector<std::vector<std::pair<int,int>>> graph(n);
+    std::vector<std::vector<std::pair<int,float>>> graph(n);
     generateComplete(graph);
     displayAdjList(graph);
 }
