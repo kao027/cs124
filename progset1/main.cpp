57f3870653cb96e82c686a9adadf189d6d655adf
Jasmine Andresol <jasmineandresol@Jasmines-Air.lan>
2025-02-22 19:49:53 -0500

    heh???

--- progset1/main.cpp
+++ progset1/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include "mst.h"
+#include <math.h>
 
 int n = 3; // number of nodes
 void addEdge(Graph &adj, int u, int v, float w){
@@ -10,7 +11,7 @@ void addEdge(Graph &adj, int u, int v, float w){
 }
 
 //graph type #1
-void generateComplete(Graph &adj){ //
+void generateComplete(Graph &adj){
     srand(time(0));
     for (int u = 0; u < n; u++){
         for (int v = u+1; v < n; v++){
@@ -20,10 +21,44 @@ void generateComplete(Graph &adj){ //
     }
 }
 
+//graph type #2
+void generateHypercube(Graph &adj){
+    srand(time(0));
+    for (int u = 0; u < n; u++){
+        for (int v = u+1; v<n; v++){
+            if (((v-u) & (v-u-1))==0){
+                float newWeight = (float)rand() / RAND_MAX;
+                addEdge(adj, u, v, newWeight);
+            }
+        }
+    }
+}
+
 //graph type #3
 void generateUnitSquare(Graph &adj){
-    
+    srand(time(0));
+     std::vector<std::pair<float,float>> coordinates(n);
+    for (int i = 0; i < n; i++){
+       float x = (float)rand() / RAND_MAX;
+       float y = (float)rand() / RAND_MAX;
+       coordinates[i] = {x,y};
+       //std::cout << "(" << coordinates[i].first << ", " << coordinates[i].second << ") " << "\n";
+    }
+    for (int u = 0; u < n; u++){
+        float ux = coordinates[u].first;
+        float uy = coordinates[u].second;
+        for (int v = u+1; v < n; v++){
+            float vx = coordinates[v].first;
+            float vy = coordinates[v].second;
+            float euclidWeight = sqrt(pow(vx-ux, 2)+pow(vy-uy, 2));
+            addEdge(adj, u, v, euclidWeight);
+        }
+    }
+
 }
+
+//graph type #4
+
 
 void displayAdjList(Graph &adj) {
     for (size_t i = 0; i < adj.size(); i++) {
